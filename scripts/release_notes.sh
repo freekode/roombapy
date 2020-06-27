cf5a99d9b9c638f36001de1710ae6e71ba50eac8
Evgeny <freekode@users.noreply.github.com>
2020-06-27 13:56:01 +0200

    Update release_notes.sh

--- scripts/release_notes.sh
+++ scripts/release_notes.sh
@@ -7,5 +7,7 @@ git fetch --tags
 # collect the commits since the last tag
 export GIT_RELEASE_NOTES="$(git log $(git describe --tags --abbrev=0)..HEAD --pretty=format:"%h %s")"
 
+::set-env name=GIT_RELEASE_NOTES::$GIT_RELEASE_NOTES
+
 # expose as env var for subsequent build steps
 envman add --key GIT_RELEASE_NOTES --value "$GIT_RELEASE_NOTES"
